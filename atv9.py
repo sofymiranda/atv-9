a8cb7821a5fed0ce6e7be22b966208d24676b5ae
sofymiranda <mirandaspfya@gmail.com>
2023-10-16 15:25:39 -0300

    atividade 9

---
+++ atv9.py
@@ -0,0 +1,7 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+n1= int (input("digite o primeiro numero inteiro: "))
+n2= int (input("digite o segundo numero inteiro: "))
+if n1 >= n2:
+    print (f"o primeiro numero: {n1} é maior ou igual a {n2}")
+else :
+    print(f"o primeiro numero: {n1} é menor que {n2}")
